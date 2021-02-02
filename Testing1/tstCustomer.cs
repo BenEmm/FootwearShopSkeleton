b477fa5b768f81cbe27243b59a79e2f10384e234
Ben Mcleod <BenMcleod95@Hotmail.co.uk>
2021-02-02 15:22:30 +0000

    Added testing and class for address management (customer management) as per the lab sheet.

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Create an instance of the class we want to create
+            clsCustomer ACustomer = new clsCustomer();
+            // Test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+    }
+}
