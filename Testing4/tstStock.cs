2d803b5ff63a5aea17b674b3b8c47ad5647d01d8
Luke Knowles <lukeknowles61@gmail.com>
2021-02-03 23:23:44 +0000

    Added testing and class for Stock Management as per week 18 practical

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
