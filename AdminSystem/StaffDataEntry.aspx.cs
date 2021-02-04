fab5b8ca79b2da2f11944e1cc027b77ed15f60a6
CaileySmith <caileyamber212@gmail.com>
2021-02-04 15:23:01 +0000

    Week 18 worksheets - added tests, added clsStaff, added interface + code for staff data entry

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click1(object sender, EventArgs e)
+    {
+        //create an instance of clsStaff
+        clsStaff AStaff = new clsStaff();
+        //capture staffId
+        AStaff.StaffID = int.Parse(txtStaffID.Text);
+        //capture fullname
+        AStaff.FullName = txtFullName.Text;
+        //capture password
+        AStaff.Password = txtPassword.Text;
+        //capture date of leaving
+        AStaff.DateOfLeaving = DateTime.Parse(txtDateOfLeaving.Text);
+        //capture position
+        AStaff.Positon = txtPositon.Text;
+        //capture full time
+        AStaff.FullTime = chkFullTime.Checked;
+        //store the staff in the session object
+        Session["AStaff"] = AStaff;
+        //nagivate to viewer page
+        Response.Redirect("StaffViewer.aspx");
     }
 }
---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //test if exists
+            Assert.IsNotNull(AStaff);
+        }
+
+        [TestMethod]
+        public void StaffIdOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Int32 TestData = 2;
+            //assign the data to the property
+            AStaff.StaffID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void FullNameOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Full Name";
+            //assign the data to the property
+            AStaff.FullName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.FullName, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Password1";
+            //assign the data to the property
+            AStaff.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Password, TestData);
+        }
+
+        [TestMethod]
+        public void DateOfLeavingOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AStaff.DateOfLeaving = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.DateOfLeaving, TestData);
+        }
+
+        [TestMethod]
+        public void PostionOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            string TestData = "Weekend Staff";
+            //assign the data to the property
+            AStaff.Positon = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.Positon, TestData);
+        }
+
+        [TestMethod]
+        public void FullTimeOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AStaff.FullTime = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStaff.FullTime, TestData);
+        }
+    }
+}
