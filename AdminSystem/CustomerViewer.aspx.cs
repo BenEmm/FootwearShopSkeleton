82dac2431ed86ef288ae72c15aa8ab24011fd090
Ben Mcleod <BenMcleod95@Hotmail.co.uk>
2021-02-04 18:07:54 +0000

    Added the code for buttons and and forms and also got the address number field working

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Create a new instance of clsCustomer
+        clsCustomer ACustomer = new clsCustomer();
 
+        // Get the data from the session object
+        ACustomer = (clsCustomer)Session["ACustomer"];
+
+        // Display the house number for this entry
+        Response.Write(ACustomer.AddressNo);
     }
 }
