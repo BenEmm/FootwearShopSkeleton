99a8c27a39c638c6db800e71a74d0904b6b448c5
Luke Knowles <lukeknowles61@gmail.com>
2021-02-04 00:48:41 +0000

    Added testing for all of my [TestingStock]table's properties as well as generating methods for each of the properties.

--- Testing4/tstStock.cs
+++ Testing4/tstStock.cs
@@ -8,10 +8,129 @@ namespace Testing4
     public class tstStock
     {
         [TestMethod]
+        
         public void InstanceOK()
         {
-            clsStock aStock = new clsStock();
-            Assert.IsNotNull(aStock);
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Test to see if it has been created
+            Assert.IsNotNull(AStock);
         }
+
+        [TestMethod]
+        public void StockIDOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property 
+            Int32 TestData = 456;
+
+            //Assign the data to the property
+            AStock.StockID = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.StockID, TestData);
+
+        }
+
+        [TestMethod]
+        public void StockDescriptionOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            String TestData = "Black and White Converse";
+
+            //Assign the data to the property
+            AStock.StockDescription = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.StockDescription, TestData);
+        }
+
+        [TestMethod]
+        public void StockColourOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            String TestData = "Black and White";
+
+            //Assign the data to the property
+            AStock.StockColour = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.StockColour, TestData);
+        }
+
+        [TestMethod]
+        public void StockAmountOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            Int32 TestData = 340;
+
+            //Assign the data to the property
+            AStock.StockAmount = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.StockAmount, TestData);
+        }
+
+        [TestMethod]
+        public void StockPriceOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            decimal TestData = 20.00M;
+
+            //Assign the data to the property
+            AStock.StockPrice = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.StockPrice, TestData);
+        }
+
+        [TestMethod]
+        public void AvailableOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            Boolean TestData = true;
+
+            //Assign the data to the property
+            AStock.Available = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.Available, TestData);
+        }
+
+        [TestMethod]
+        public void DateOrderedOK()
+        {
+            //Create an instance of a class we want to create
+            clsStock AStock = new clsStock();
+
+            //Create test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+
+            //Assign the data to the property
+            AStock.DateAdded = TestData;
+
+            //Test to see if the two values are the same
+            Assert.AreEqual(AStock.DateAdded, TestData);
+
+        }
+
     }
 }
