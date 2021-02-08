bfed13ab64e47e96e215b1f1dd5346b464734f18
CaileySmith <caileyamber212@gmail.com>
2021-02-08 14:46:25 +0000

    Week 19 lab creating staff find method

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,11 +4,94 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public int StaffID { get; set; }
-        public string FullName { get; set; }
-        public string Password { get; set; }
-        public DateTime DateOfLeaving { get; set; }
-        public string Positon { get; set; }
-        public bool FullTime { get; set; }
+        private int mStaffID;
+        public int StaffID
+        {
+            get
+            {
+                return mStaffID;
+            }
+            set
+            {
+                mStaffID = value;
+            }
+        }
+
+        private string mFullName;
+        public string FullName
+        {
+            get
+            {
+                return mFullName;
+            }
+            set
+            {
+                mFullName = value;
+            }
+        }
+
+        private string mPassword;
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+
+        private DateTime mDateOfLeaving;
+        public DateTime DateOfLeaving
+        {
+            get
+            {
+                return mDateOfLeaving;
+            }
+            set
+            {
+                mDateOfLeaving = value;
+            }
+        }
+
+        private string mPosition;
+        public string Positon
+        {
+            get
+            {
+                return mPosition;
+            }
+            set
+            {
+                mPosition = value;
+            }
+        }
+
+        private bool mFullTime;
+        public bool FullTime
+        {
+            get
+            {
+                return mFullTime;
+            }
+            set
+            {
+                mFullTime = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            mStaffID = 21;
+            mFullName = "Full Name";
+            mPassword = "Password1";
+            mDateOfLeaving = Convert.ToDateTime("16/09/2020");
+            mPosition = "Weekend Staff";
+            mFullTime = false;
+            //always return true
+            return true;
+        }
     }
 }
--- Testing2/tstStaff.cs
+++ Testing2/tstStaff.cs
@@ -93,5 +93,152 @@ namespace Testing2
             //test to see that the two values are the same
             Assert.AreEqual(AStaff.FullTime, TestData);
         }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result sof validation
+            Boolean found = false;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //test to see if result is true
+            Assert.IsTrue(found);
+        }
+
+        [TestMethod]
+        public void StaffIDFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.StaffID != 21)
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void FullNameFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.FullName != "Full Name")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void PasswordFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.Password != "Password1")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void DateOfLeavingFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.DateOfLeaving != Convert.ToDateTime("16/09/2020"))
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void PositionFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.Positon != "Weekend Staff")
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void FullTimeFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AStaff = new clsStaff();
+            //boolean variable to store the result of validation
+            Boolean found = false;
+            //boolean variable to record if data is okay
+            Boolean OK = true;
+            //create some test data to use with the method 
+            Int32 StaffID = 21;
+            //invoke the method 
+            found = AStaff.Find(StaffID);
+            //check the staffID
+            if (AStaff.FullTime != false)
+            {
+                OK = false;
+            }
+            //test to see if result is true
+            Assert.IsTrue(OK);
+        }
     }
 }
