db90f7adadef9071710a848e2040695fb4372e5b
McSmallFries <m-handley@live.co.uk>
2021-02-17 20:12:39 +0000

    Added: - Fields & properties to clsBrand - Test cases for clsBrand. - A start on the presentation layer. - CSS to BrandsDataEntry. - C# code to control front end pages.

---
+++ ClassLibrary/clsBrand.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace ClassLibrary {
+
+    public class clsBrand
+    {
+        //encapsulated fields 
+        private int brandId;
+        private string brandName;
+        private int topProduct;
+        private int latestProduct;
+        private DateTime lastRestock;
+        private double avgPrice;
+        private bool isListed;
+        
+        //properties and access modifiers
+        public int BrandID { get { return brandId; } private set { brandId = value; } } // cannot set this outside of class, the DB assigns the number, this class will fetch the num to be set here.
+        public string BrandName { get { return brandName; } set { brandName = value; } }
+        public int TopProduct { get { return topProduct; } set { topProduct = value; } }
+        public int LatestProduct { get { return latestProduct; } set { latestProduct = value; } }
+        public DateTime LastRestock { get { return lastRestock; } set { lastRestock = value; } }
+        public double AvgPrice { get { return avgPrice; } set { avgPrice = value; } }
+        public bool IsListed { get { return isListed; } set { isListed = value; } }
+
+        public clsBrand()
+        {
+            BrandID = 0;
+            BrandName = "";
+            TopProduct = 0;
+            LatestProduct = 0;
+            LastRestock = new DateTime();
+            AvgPrice = 0.00;
+            IsListed = false;
+        }
+
+        // full constructor
+        public clsBrand(string name, int tProduct, int lProduct, DateTime restock, bool isListed) 
+        {
+            BrandName = name;
+            TopProduct = tProduct;
+            LatestProduct = lProduct;
+            LastRestock = restock;
+            AvgPrice = calculateAvgPrice();
+            IsListed = isListed;
+            AddToDatabase(this);
+            BrandID = FindBrandID(); // the num of the last db entry
+        }
+
+
+        public void Delete()
+        {
+            clsDataConnection connection = new clsDataConnection();
+            connection.AddParameter("@BrandID", this.BrandID);
+            connection.Execute("sproc_Brand_Delete");
+        }
+        public void AddToDatabase(clsBrand brand)  
+        {
+            // converts a DateTime variable to sql DATE format.
+            string sqlRestockDate = "'" + brand.LastRestock.Year + "-" + brand.LastRestock.Month + "-" + brand.LastRestock.Day + "'";
+            clsDataConnection connection = new clsDataConnection();
+            connection.AddParameter("@BrandName", brand.BrandName);
+            connection.AddParameter("@TopProduct", brand.TopProduct);
+            connection.AddParameter("@LatestProduct", brand.LatestProduct);
+            connection.AddParameter("@LastRestock ", sqlRestockDate);
+            connection.AddParameter("@AvgPrice", brand.AvgPrice);
+            connection.AddParameter("@IsListed", brand.IsListed);
+            connection.Execute("sproc_Brand_Add");
+        }
+
+        public double calculateAvgPrice()  
+        {
+            return 0.00;
+        }
+
+        private int FindBrandID()
+        {
+            clsDataConnection connection = new clsDataConnection();
+            return connection.Count;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || this.GetType().Name != obj.GetType().Name)
+            {
+                return false;
+            }
+
+            clsBrand other = (clsBrand)obj;
+
+            return this.BrandID == other.BrandID;
+        }
+
+        public override int GetHashCode()
+        {
+            return BrandID.GetHashCode() + BrandName.GetHashCode() * 29;
+        }
+    }
+
+}
---
+++ Testing5/tstBrand.cs
@@ -0,0 +1,46 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+
+    public class tstBrand
+    {
+        clsBrand brand = new clsBrand("Nike", 2, 2, new System.DateTime(2020, 10, 11), true);
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsBrand notABrand = new clsBrand();
+            Assert.IsNotNull(notABrand);
+
+            // test full constructor
+            Assert.IsNotNull(brand);
+        }
+
+
+        // test fields
+        [TestMethod]
+        public void PrimaryKeyOK() { 
+        
+            int testBrandID = brand.BrandID;
+            Assert.IsTrue(testBrandID  > 0); // means it was assigned a number using sproc
+        }
+
+        [TestMethod] 
+        public void TestBrandName()
+        {
+            string TestData = "Nike";
+            Assert.AreEqual(brand.BrandName, TestData);
+        }
+
+        public void TestTopProduct()
+        {
+            int TestData = 1;
+            Assert.AreEqual(brand.TopProduct, TestData);
+        }
+    }
+}
+
+
