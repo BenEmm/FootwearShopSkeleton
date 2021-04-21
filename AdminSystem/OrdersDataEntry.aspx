8a186d92461294c1041171beeaadeb3937ad6054
Eseosa Emovon <P2508279@my365.dmu.ac.uk>
2021-04-21 18:19:42 +0100

    updated Github with work from local PC

--- AdminSystem/OrdersDataEntry.aspx
+++ AdminSystem/OrdersDataEntry.aspx
@@ -12,7 +12,8 @@
         <div>
         </div>
         <asp:Label ID="lblOrderNo" runat="server" Text="Order Number" width="47px"></asp:Label>
-        <asp:TextBox ID="txtOrderNo" runat="server" Height="25px" style="margin-left: 40px" Width="178px"></asp:TextBox>
+        <asp:TextBox ID="txtOrderNo" runat="server" Height="25px" style="margin-left: 40px" Width="184px"></asp:TextBox>
+        <asp:Button ID="btnfind" runat="server" OnClick="Button1_Click" style="margin-left: 36px" Text="Find" Width="150px" />
         <p>
             <asp:Label ID="lblCustomerName" runat="server" Text="Customer Name" width="47px"></asp:Label>
             <asp:TextBox ID="txtCustomerName" runat="server" style="margin-left: 38px" Width="178px" height="25px"></asp:TextBox>
@@ -29,7 +30,7 @@
             <asp:Label ID="lblQuantity" runat="server" Text="Quantity" width="47px"></asp:Label>
             <asp:TextBox ID="txtQuantity" runat="server" style="margin-left: 42px" height="25px" width="178px"></asp:TextBox>
         </p>
-        <p>
+        <p aria-checked="undefined">
             <asp:Label ID="lblShippingDate" runat="server" Text="Shipping Date" width="47px"></asp:Label>
             <asp:TextBox ID="txtShippingDate" runat="server" style="margin-left: 42px" height="25px" width="178px"></asp:TextBox>
         </p>
--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -4,7 +4,7 @@ namespace ClassLibrary
 {
     public class clsOrder
     {
-        private Int32 mOrderNumber;
+        private int mOrderNumber;
 
         public int OrderNumber
         {
@@ -18,7 +18,7 @@ namespace ClassLibrary
                 mOrderNumber = value;
             }
         }
-       private string mTrackingNumber;
+        private string mTrackingNumber;
 
         public string TrackingNumber
         {
@@ -50,7 +50,7 @@ namespace ClassLibrary
         }
 
         private bool mShipped;
-        public bool Shipped 
+        public bool Shipped
         {
             get
             {
@@ -95,7 +95,8 @@ namespace ClassLibrary
 
         private string mEmail;
 
-        public string Email {
+        public string Email
+        {
             get
             {
                 return mEmail;
@@ -105,7 +106,7 @@ namespace ClassLibrary
             {
                 mEmail = value;
             }
-         }
+        }
 
         private decimal mTotalPrice;
 
@@ -129,15 +130,128 @@ namespace ClassLibrary
 
         public bool Find(int OrderNumber)
         {
-            mOrderNumber = 21;
-            mTotalPrice = 50;
-            mQuantity = 10;
-            mEmail = "TestEmail@Test.com";
-            mCustomerName = "Jack James";
-            mTrackingNumber = "XXXXXXX";
-            mShippingDate = Convert.ToDateTime("16/09/2021");
-            mShipped = true;
-            return true;
+
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the address no to search
+            DB.AddParameter("@OrderNumber", OrderNumber);
+            //Execute procedure 
+            DB.Execute("sproc_tblOrder_FilterByOrderNumber");
+            //if one record is found (Either 1 or 0)
+            if (DB.Count == 1)
+            {
+                //Copy the data from the database to the private data members
+                mOrderNumber = Convert.ToInt32(DB.DataTable.Rows[0]["OrderNumber"]);
+                mTotalPrice = Convert.ToDecimal(DB.DataTable.Rows[0]["TotalPrice"]);
+                mQuantity = Convert.ToInt32(DB.DataTable.Rows[0]["Quantity"]);
+                mEmail = Convert.ToString(DB.DataTable.Rows[0]["Email"]);
+                mCustomerName = Convert.ToString(DB.DataTable.Rows[0]["CustomerName"]);
+                mTrackingNumber = Convert.ToString(DB.DataTable.Rows[0]["TrackingNumber"]);
+                mShippingDate = Convert.ToDateTime(DB.DataTable.Rows[0]["ShippingDate"]); ;
+                mShipped = Convert.ToBoolean(DB.DataTable.Rows[0]["Shipped"]);
+                //Return everything that worked 
+                return true;
+            }
+
+            //If no record was found 
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
+        }
+
+
+        public string Valid(string orderNumber, string customerName, string email, string quantity, string shippingDate, string trackingNumber)
+        {
+
+            String Error = "";
+            DateTime DateTemp;
+
+            if (orderNumber.Length == 0)
+            {
+                // record the error
+                Error = Error + "The order no may not be blank : ";
+            }
+            //if the order no is greater than 6 characters
+            if (orderNumber.Length > 6)
+            {
+                //record the error
+                Error = Error + "The order no must be less than 6 characters : ";
+            }
+
+            try
+            {
+                //copy the orderDate value to the DateTemp variable
+                DateTemp = Convert.ToDateTime(shippingDate);
+                if (DateTemp < DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the past : ";
+                }
+                //check to see if the date is greater than today's date 
+                if (DateTemp > DateTime.Now.Date)
+                {
+                    //record the error
+                    Error = Error + "The date cannot be in the future :";
+                }
+            }
+            catch
+            {
+                //record the error 
+                Error = Error + "The date was not a valid date : ";
+            }
+            //is the customer name blank
+            if (customerName.Length == 0)
+            {
+                //record the error
+                Error = Error + "the customer name may not be blank :";
+            }
+            //if the customer name is too long
+            if (customerName.Length > 50)
+            {
+                //record the error 
+                Error = Error + "The customer name must be less than 50 characters : ";
+            }
+            //is the customer email blank
+            if (email.Length == 0)
+            {
+                //record the error
+                Error = Error + "the customer email may not be blank :";
+            }
+            //if the customer email is too long
+            if (email.Length > 50)
+            {
+                //record the error 
+                Error = Error + "The customer email must be less than 50 characters : ";
+            }
+
+            //if the TrackingNo is blank
+            if (trackingNumber.Length == 0)
+            {
+                // record the error
+                Error = Error + "The tracking no may not be blank : ";
+            }
+            //if the tracking no is greater than 6 characters
+            if (trackingNumber.Length > 6)
+            {
+                //record the error
+                Error = Error + "The tracking no must be less than 6 characters : ";
+            }
+            //if the Quantity is blank
+            if (quantity.Length == 0)
+            {
+                // record the error
+                Error = Error + "The tracking no may not be blank : ";
+            }
+            //if the quantity is greater than 3 characters
+            if (quantity.Length > 3)
+            {
+                //record the error
+                Error = Error + "The quantity must be less than 3 characters : ";
+            }
+            //return any error messages
+            return Error;
         }
     }
 }
